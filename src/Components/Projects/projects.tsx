50cca78645a992acd9edc4c14be05df451254940
Johannes Mohr <johannes_mohr@yahoo.de>
2022-09-29 11:20:21 +0200

    slider slowly coming together

--- src/Components/Projects/projects.tsx
+++ src/Components/Projects/projects.tsx
@@ -14,7 +14,7 @@ export default function Projects({ clickOnProjects }) {
         superLargeDesktop: {
             // the naming can be any, depends on you.
             breakpoint: { max: 4000, min: 3000 },
-            items: 1,
+            items: 3,
         },
         desktop: {
             breakpoint: { max: 3000, min: 1024 },
@@ -34,63 +34,55 @@ export default function Projects({ clickOnProjects }) {
             {/* <h1 className="heading">Projects</h1> */}
             <div className="projectsWrapper">
                 <div id="closeProjects" onClick={clickOnProjects}></div>
-                <div className="sliderWrapper">
-                    <Carousel responsive={responsive}>
-                        <div
-                            data-aos="fade-right"
-                            id="soundsound"
-                            className="projectContainer"
-                        >
+                {/* <div className="sliderWrapper"> */}
+                <Carousel className="sliderWrapper" responsive={responsive}>
+                    <div
+                        data-aos="fade-right"
+                        id="soundsound"
+                        className="projectContainer"
+                    >
+                        <div className="projectVideoWrapper">
                             <img src="img/planetSkills.png" alt="SOSO"></img>
-                            <p className="projectInfo">
-                                Lorem ipsum dolor sit amet, consetetur
-                                sadipscing elitr, sed diam nonumy eirmod tempor
-                                invidunt ut labore et dolore magna aliquyam
-                                erat, sed diam voluptua. At vero eos et accusam
-                                et justo duo dolores et ea rebum. Stet clita
-                                kasd gubergren, no sea takimata sanctus est
-                                Lorem ipsum dolor sit amet. Lorem ipsum dolor
-                                sit amet, consetetur sadipscing elitr, sed diam
-                                nonumy eirmod tempor invidunt ut labore et
-                                dolore magna aliquyam erat, sed diam voluptua.
-                                At vero eos et accusam et justo duo dolores et
-                                ea rebum. Stet clita kasd gubergren, no sea
-                                takimata sanctus est Lorem ipsum dolor sit amet.
-                            </p>
+                        </div>
+                        <div className="projectInfoWrapper">
                             <div className="projectInfo">
                                 <h2 className="projectName">PROJECT 3</h2>
+                                <p className="projectInfo">
+                                    Lorem ipsum dolor sit amet, consetetur
+                                    sadipscing elitr, sed diam nonumy eirmod
+                                    tempor invidunt ut labore et dolore magna
+                                    aliquyam erat, sed diam voluptua. At vero
+                                    eos et accusam et justo duo dolores et ea
+                                    rebum. Stet clita kasd gubergren, no sea
+                                    takimata sanctus est Lorem ipsum dolor sit
+                                    amet.
+                                </p>
                             </div>
                         </div>
-                        {/* ===== ZOME ====== */}
-                        <div
-                            data-aos="fade-right"
-                            id="zone"
-                            className="projectContainer"
-                        >
-                            <img
-                                src="img/planetContact.png"
-                                alt="projectImg"
-                            ></img>
+                    </div>
+                    {/* ===== ZOME ====== */}
+                    {/* <div
+                        data-aos="fade-right"
+                        id="zone"
+                        className="projectContainer"
+                    >
+                        <img src="img/planetContact.png" alt="projectImg"></img>
 
-                            {/* <h2 className="projectName">PROJECT 2</h2> */}
-                        </div>
-                        {/* ===== PROJECT 3 ====== */}
+                        {/* <h2 className="projectName">PROJECT 2</h2> */}
+                    {/* </div>  */}
+                    {/* ===== PROJECT 3 ====== */}
 
-                        <div
-                            data-aos="fade-right"
-                            id="zone"
-                            className="projectContainer"
-                        >
-                            <img
-                                src="img/planetResume.png"
-                                alt="projectImg"
-                            ></img>
+                    {/* <div
+                        data-aos="fade-right"
+                        id="zone"
+                        className="projectContainer"
+                    >
+                        <img src="img/planetResume.png" alt="projectImg"></img>
 
-                            {/* <h2 className="projectName">PROJECT 3</h2> */}
-                        </div>
-                    </Carousel>
-                    ;
-                </div>
+                        {/* <h2 className="projectName">PROJECT 3</h2> 
+                    </div> */}
+                </Carousel>
+                ;{/* </div> */}
             </div>
         </div>
     );
