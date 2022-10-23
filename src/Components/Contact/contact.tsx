857f495f5e778040e6c7f5242eeec587cd001eac
Johannes Mohr <johannes_mohr@yahoo.de>
2022-10-23 13:37:09 +0200

    secrets json

--- src/Components/Contact/contact.tsx
+++ src/Components/Contact/contact.tsx
@@ -1,10 +1,64 @@
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 import "./contact.css";
+const keys = require("../../secrets.json");
 
 export default function Contact({ closePopup }) {
+    const form = useRef();
+    console.log(
+        "🟢",
+        keys.MY_SERVICE_ID,
+        keys.MY_TEMPLATE_ID,
+        keys.MY_PUBLIC_KEY
+    );
+
+    const sendEmail = (e): void => {
+        e.preventDefault();
+
+        emailjs
+            .sendForm(
+                keys.MY_SERVICE_ID,
+                keys.MY_TEMPLATE_ID,
+                form.current,
+                keys.MY_PUBLIC_KEY
+            )
+            .then(
+                (result) => {
+                    console.log(result.text);
+                    e.target.reset();
+                    console.log("MESSAGE SENT");
+                },
+                (error) => {
+                    console.log(error.text);
+                    console.log(
+                        "🟢",
+                        keys.MY_SERVICE_ID,
+                        keys.MY_TEMPLATE_ID,
+                        keys.MY_PUBLIC_KEY
+                    );
+                    console.log("MESSAGE FAILED TO SENT");
+                }
+            );
+    };
     return (
         <div className="contactWrapper">
-         
+            <img
+                onClick={closePopup}
+                id="closeProjects"
+                src="img/closeIcon.png"
+                alt="Close"
+            />
+            <h1 className="contactHeading">CONTACT ME</h1>
+            {/* <div id="trenner"></div> */}
+            <form className="contactForm" ref={form} onSubmit={sendEmail}>
+                <label>Name</label>
+                <input type="text" name="user_name" />
+                <label>Email</label>
+                <input type="email" name="user_email" />
+                <label>Message</label>
+                <textarea name="message" />
+                <input id="sendButton" type="submit" value="SEND OF 🪐" />
+            </form>
         </div>
     );
 }
