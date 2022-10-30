41f0de7c17a54c318a6218dd6e69acb1f5e68c81
Johannes Mohr <johannes_mohr@yahoo.de>
2022-10-30 11:36:01 +0100

    fixing dotenv

--- src/Components/Contact/contact.tsx
+++ src/Components/Contact/contact.tsx
@@ -1,34 +1,30 @@
 import React, { useRef } from "react";
 import emailjs from "@emailjs/browser";
 import "./contact.css";
-const keys = require("../../secrets.json");
-// require("dotenv").config();
+// const keys = require("../../secrets.json");
+require("dotenv").config();
 
 export default function Contact({ closePopup }: any) {
     const form = useRef<HTMLFormElement>(null);
+    const serviceId: any = process.env.REACT_APP_SERVICE_ID;
+    const templateId: any = process.env.REACT_APP_TEMPLATE_ID;
+    const publicKey: any = process.env.REACT_APP_PUBLIC_KEY;
 
     const sendEmail = (e: any): void => {
         e.preventDefault();
 
-        emailjs
-            .sendForm(
-                keys.REACT_APP_SERVICE_ID,
-                keys.REACT_APP_TEMPLATE_ID,
-                form.current!,
-                keys.REACT_APP_PUBLIC_KEY
-            )
-            .then(
-                (result: { text: any }) => {
-                    console.log(result.text);
-                    e.target.reset();
-                    alert("MESSAGE SENT");
-                },
-                (error: { text: any }) => {
-                    console.log(error.text);
+        emailjs.sendForm(serviceId, templateId, form.current!, publicKey).then(
+            (result: { text: any }) => {
+                console.log(result.text);
+                e.target.reset();
+                alert("MESSAGE SENT");
+            },
+            (error: { text: any }) => {
+                console.log(error.text);
 
-                    alert("MESSAGE FAILED TO SENT");
-                }
-            );
+                alert("MESSAGE FAILED TO SENT");
+            }
+        );
     };
     return (
         <div className="contactWrapper">
